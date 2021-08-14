82ef81b9030ef065342ff6b2fb8fd1cfa865b114
tomohikoseven <tomohikoseven0905@gmail.com>
2021-08-14 15:38:52 +0900

    improve 7kyu Two To One.

--- 7kyu/TwoToOne/index.js
+++ 7kyu/TwoToOne/index.js
@@ -1,15 +1,5 @@
 function longest(s1, s2) {
-  let s_con = (s1 || "") + (s2 || "");
-  let s = {};
-  let tmpstr = [];
-
-  for (let i = 0; i < s_con.length; i++) {
-    s[s_con[i]] = s_con[i];
-  }
-  for (let key in s) {
-    tmpstr.push(key);
-  }
-  return tmpstr.sort((a, b) => (a > b ? 1 : -1)).join("");
+  return [...new Set(s1 + s2)].sort().join("");
 }
 
 module.exports = longest;
